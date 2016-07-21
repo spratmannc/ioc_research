4946926a45953d9eeb2e4d206bb9df59067d2b45
Roger Spratley <SPRATLRX@legal.regn.net>
2016-07-21 11:57:04 -0400

    Add project files.

---
+++ Reseach.Resolution/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reseach.Resolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
